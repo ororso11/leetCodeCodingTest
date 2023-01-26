871302f2777b6cfdf419444418f9dd67958867af
ororso11 <ororso135055@gmail.com>
2023-01-26 23:03:38 +0900

    1. leetcode 문제 풀이

---
+++ src/lc10.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class lc10 {
+    public static void main(String[] args) {
+        int[] candies = {2,8,7};
+        int extraCandies = 1;
+        boolean[] resBoolean = new boolean[candies.length];
+        List<Boolean> res = new ArrayList<>();
+
+        int max = Arrays.stream(candies).max().getAsInt();
+
+        for(int i = 0; i < candies.length;i++) {
+            if( candies[i] + extraCandies >= max ) {
+                resBoolean[i] = true;
+            } else {
+                resBoolean[i] = false;
+            }
+            res.add(resBoolean[i]);
+        }
+
+        System.out.println(res);
+    }
+}
